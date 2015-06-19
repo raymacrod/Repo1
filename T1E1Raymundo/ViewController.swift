a82570dc8fe6dcffc61608742f62eb62b864e838
Usuario <raymacrod@MacMini.local>
2015-06-18 23:44:55 -0600

    Initial Commit

---
+++ T1E1Raymundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  T1E1Raymundo
+//
+//  Created by Usuario on 18/06/15.
+//  Copyright (c) 2015 Usuario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
