c2bee9fd4ffbb1822eb7f641a20dbabb93c62434
Usuario <raymacrod@MacMini.local>
2015-06-18 23:51:05 -0600

    Se añade un nuevo File de tipo Cocoa Touch Class con el nombre de NewController de la clase ViewController

---
+++ T1E1Raymundo/NewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewController.swift
+//  T1E1Raymundo
+//
+//  Created by Usuario on 18/06/15.
+//  Copyright (c) 2015 Usuario. All rights reserved.
+//
+
+import UIKit
+
+class NewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
